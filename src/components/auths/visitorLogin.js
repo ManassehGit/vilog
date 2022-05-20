c02401d95ce0d16dbb51cb07992dff6282d02d0d
Manasseh <manasseh.sarfo@amalitech.org>
2022-05-20 05:04:30 +0100

    create login and signup page for visitors

--- src/components/auths/visitorLogin.js
+++ src/components/auths/visitorLogin.js
@@ -2,7 +2,57 @@ import React from 'react'
 
 const VisitorLogin = () => {
   return (
-    <div>VisitorLogin</div>
+    <div>
+
+<img src='viLog.png' alt='ViLog Logo' width="200" className="m-4"/>
+<div class="d-flex justify-content-center">
+        
+        <div class="jumbotron jumbotron-fluid border col-md-6">
+          <div class="container">
+            <h1 class="display-3">Host/Admin Login</h1>
+            <p class="lead">Welcome to ViLog</p>
+            <hr class="my-2" />
+            <p>
+
+            <div class="d-flex align-items-start">
+  <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+    <button class="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</button>
+    <button class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</button>
+    
+  </div>
+  <div class="tab-content" id="v-pills-tabContent">
+    <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab" tabindex="0">
+      lsdfsdf
+    </div>
+    <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab" tabindex="0">
+      fsdfsdf
+    </div>
+    
+  </div>
+</div>
+
+            </p>
+          </div>
+        </div>
+      </div>
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
   )
 }
 
