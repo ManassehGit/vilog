7e45a473bc45c15e8cb9a0166abc811c31c3216d
Manasseh <manasseh.sarfo@amalitech.org>
2022-05-24 04:53:44 +0100

    modify add js, pages fully routed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HostLogin from './components/auths/hostLogin';
 import VisitorLogin from './components/auths/visitorLogin';
 import HostPage from './components/host/hostPage';
 import Dashboard from './components/dashboard/dashboard';
+import HostSignup from './components/auths/hostSignup';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />}/>
       <Route path="login" element={<HostLogin />} />
+      <Route path="signup" element={<HostSignup />} />
       <Route path="loginSignup" element={<VisitorLogin />} />
       <Route path="host" element={<HostPage />} />
       <Route path="dashbord" element={<Dashboard />} />
