0ce256d10f928abe6ab7220bacef5efc21bc2e1d
Manasseh <manasseh.sarfo@amalitech.org>
2022-05-25 03:20:09 +0100

    start with dashboard component for admin and host

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import {Admin, Resource} from 'react-admin';
+import restProvider from 'ra-data-simple-rest';
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div>
+      <Admin dataProvider={restProvider('http://localhost:3000')}>
+        <Resource name="visitors" />
+      </Admin>
+    </div>
   )
 }
 
