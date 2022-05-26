0f83ad70b09269623c7a2ed80c88b3644844332c
Manasseh <manasseh.sarfo@amalitech.org>
2022-05-26 19:16:35 +0100

    modify visit login component to update state with details for each visitor login

--- src/components/auths/emailUsernameField.js
+++ src/components/auths/emailUsernameField.js
@@ -1,8 +1,12 @@
 import React, {useState, useRef} from 'react'
-import emailjs from '@emailjs/browser';
+import swal from 'sweetalert2'; 
+import date from 'date-and-time';
+import { useSelector, useDispatch } from 'react-redux';
+import { addVisitor } from '../../store/visitorSlice';
 
-const EmailUsernameField = ({text}) => {
+const EmailUsernameField = ({text, choice}) => {
   const [input, setInput] = useState("");
+  const dispatch = useDispatch();
 
   const handleText = ({target}) => {
     console.log(target, target.value);
@@ -12,26 +16,37 @@ const EmailUsernameField = ({text}) => {
 
   const form = useRef();
 
-  const sendEmail = (e) => {
+  const handleLogin = (e) => {
     e.preventDefault();
 
-    let public_key = "eNDvYA3W7CSZFspDG";
+    let now = new Date();
+    let day = date.format(now, "YYYY-MM-DD");
+    let time = date.format(now, "HH:mm:ss");
+    try{
+      dispatch(addVisitor({email: input, dateCurrent: day, timeIn: time, isLoggedIn: true, timeOut: null, loginMethod: choice}));
+      document.querySelector("#emailUsername").value = "";
+          
+          swal.fire({
+            title: `${input} logged in successfully`,
+            text: "Welcome to the organization, feel at home",
+            icon: "success",
+            confirmButtonText: "Ok",
+          });
+    }catch(err){
+      console.log(err)
+    }
+    
 
-    emailjs.sendForm('qr_code', 'qr_login', form.current, public_key)
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
+    
   };
 
     let typeVal = text==="Email"? "email": "text";
   return (
     <div>
-        <form ref={form} onSubmit={sendEmail}>
-        <label for="staticEmail" class="col-sm-8 col-form-label mt-4">{text}</label>
+        <form ref={form} onSubmit={handleLogin}>
+        <label for="emailUsername" class="col-sm-8 col-form-label mt-4">{text}</label>
           <div class="col-sm-10">
-            <input type={typeVal} class="form-control" id="staticEmail" placeholder={`Kindly enter here ...`} onChange={handleText}/>
+            <input type={typeVal} class="form-control" id="emailUsername" placeholder={`Kindly enter here ...`} onChange={handleText}/>
             <button type="submit" class="btn btn-outline-primary mt-2">Send</button>
           </div>
         </form>
