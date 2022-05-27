65e1cf4aa42c9f40db9ab4ff383081dc8f3d9007
Manasseh <manasseh.sarfo@amalitech.org>
2022-05-27 13:27:06 +0100

    test api post request with button click

--- src/components/host/listVisitors.js
+++ src/components/host/listVisitors.js
@@ -1,9 +1,24 @@
 import React, {useEffect, useState} from 'react'
 import {useSelector} from 'react-redux'
+import axios from 'axios';
+
 
 const ListVisitors = () => {
 
   let visitors = useSelector(state => state.visitors.visitorEmails);
+
+  const handleClick = async () => {
+    try{
+      let result = await axios.get("/users")
+
+      let temp = await result.data;
+      console.log(temp)
+      return temp
+
+    }catch(err){
+      console.log(err)
+    }
+  }
   
   
   useEffect(() => {
@@ -14,8 +29,9 @@ const ListVisitors = () => {
   return (
     <div>
         <ul>
-            {visitors.map(visitor => <li>{visitor}</li>)}
+            {visitors.map((visitor, id) => <li key={id}>{visitor}</li>)}
         </ul>
+        <button onClick={handleClick}>Check</button>
     </div>
   )
 }
