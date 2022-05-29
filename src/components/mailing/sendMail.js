6fe052425319422a16aeb72db179aa2d8d6fc105
Manasseh <manasseh.sarfo@amalitech.org>
2022-05-29 21:29:33 +0100

    configure visitor send QR code mail function and test

--- src/components/mailing/sendMail.js
+++ src/components/mailing/sendMail.js
@@ -1,36 +1,52 @@
-"use strict";
+// "use strict";
 const nodemailer = require("nodemailer");
+const {google} = require('googleapis');
+const res = require("express/lib/response");
 
-// async..await is not allowed in global scope, must use a wrapper
-async function main() {
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  let testAccount = await nodemailer.createTestAccount();
+const CLIENT_ID = '351684500881-6kjjab649ukd0cs1r3booej83dt2k0hm.apps.googleusercontent.com';
+const CLIENT_SECRET = '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';
+const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
+const REFRESH_TOKEN = '1//04Q7s6rrbDPPuCgYIARAAGAQSNwF-L9IrHH1UcdK-MsPbIeE4Z672xlTpmaS1yw-sgkrfONhRehletHenRxGtbXDYXdMBQC6juWE';
 
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    service:'gmail',
-    auth: {
-      user: "vilogsys@gmail.com", // generated ethereal user
-      pass: "viLog@gmail", // generated ethereal password
-    },
-  });
+ const  oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
 
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: '"ViLog" <vilogsys@gmail.com>', // sender address
-    to: "manasseh.sarfo@amalitech.org", // list of receivers
-    subject: "Kindly Login via QR", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world</b>", // html body
-  });
+ oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN})
 
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+ async function sendMail () {
+    try{
+      const accessToken = await oAuth2Client.getAccessToken();
 
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-}
+      const transport = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          type: 'OAuth2',
+          user: 'vilogsys@gmail.com',
+          clientId: CLIENT_ID,
+          clientSecret: CLIENT_SECRET,
+          refreshToken: REFRESH_TOKEN,
+          accessToken: accessToken
+        }
+      })
 
-main().catch(console.error);
+      const mailOptions = {
+        from: 'ViLog System <vilogsys@gmail.com>',
+        to: 'manasseh.sarfo@amalitech.org',
+        subject: 'Testing the send Mail function',
+        text: 'Hello there',
+        html: '<h1><code>Welcome to the page</code></h1>'
+      }
+
+      const result = transport.sendMail(mailOptions);
+      return result;
+    }catch(error){
+      return error;
+    }
+ }
+
+
+//  export const sendQRMail = async () => {
+
+//  }
+
+ sendMail().then(result => console.log('Email sent', result))
+ .catch(error => console.log(error.message));
