858291ac79d8689979927a1b408340db20047dec
Manasseh <manasseh.sarfo@amalitech.org>
2022-06-03 16:47:59 +0100

    update on the dashboard route

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <Route path="signup" element={<HostSignup />} />
       <Route path="loginSignup" element={<VisitorLogin />} />
       <Route path="host" element={<HostPage />} />
-      <Route path="dashboard" element={<Dashboard />} />
+      <Route path="dashboard/*" element={<Dashboard />} />
       
     </Routes>
   </Router>
