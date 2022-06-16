b5d6c7ddc78411ce76d08d52a33bbe9f220f88b5
Manasseh <manasseh.sarfo@amalitech.org>
2022-06-16 15:38:40 +0100

    update on dashboard

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Home from './components/home/home';
+import Home from './components/home/home';
 import HostLogin from './components/auths/hostLogin';
 import VisitorLogin from './components/auths/visitorLogin';
 import HostPage from './components/host/hostPage';
-// import Dashboard from './components/dashboard/dashboard';
+import Dashboard from './components/dashboard/dashboard';
 import HostSignup from './components/auths/hostSignup';
 
 import Welcome from './components/welcomePage/welcome';
 import VisitorMain from './components/visitorMain/visitorMain';
-// import Qrreader from './components/qrreader/qrreader';
 import VisitorQRLogin from './components/visitorSigning/visitorQRSigning';
 import VisitorEmailLogin from './components/visitorSigning/visitorEmailLogin';
 import VisitorGuest from './components/visitorSigning/visitorGuest';
@@ -26,12 +25,12 @@ function App() {
       <Route path="visitorguest" element={<VisitorGuest />}/>
       <Route path="visitorlogout" element={<VisitorLogout />}/>
       
-      {/* <Route path="start" element={<Home />}/> */}
+      <Route path="start" element={<Home />}/>
       <Route path="login" element={<HostLogin />} />
       <Route path="signup" element={<HostSignup />} />
       <Route path="loginSignup" element={<VisitorLogin />} />
       <Route path="host" element={<HostPage />} />
-      {/* <Route path="dashboard/*" element={<Dashboard />} /> */}
+      <Route path="dashboard/*" element={<Dashboard />} />
       <Route path="welcome/*" element={<Welcome />} />
       
     </Routes>
--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,42 +1,10 @@
-/*!
-
-=========================================================
-* Light Bootstrap Dashboard React - v2.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./assets/css/animate.min.css";
-import "./assets/scss/light-bootstrap-dashboard-react.scss?v=2.0.0";
-import "./assets/css/demo.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-
-import AdminLayout from "layouts/Admin.js";
 
 import React from 'react'
 
 const Dashboard = () => {
   return (
     <div>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
-      <Route path="/" element={ <Navigate to="/admin/dashboard" />} />
-    </Routes>
-  </BrowserRouter>
+      Work on dashboard
     </div>
   )
 }
