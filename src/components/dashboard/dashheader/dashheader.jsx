b06be6c03382c79285270302e546bc927b65de43
Manasseh <manasseh.sarfo@amalitech.org>
2022-07-03 04:45:07 +0100

    header component fixed

--- src/components/dashboard/dashheader/dashheader.jsx
+++ src/components/dashboard/dashheader/dashheader.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const dashheader = () => {
   return (
-    <div class="d-sm-flex justify-content-between align-items-center mb-4">
-        <h3 class="text-dark mb-0">Dashboard</h3><a className="btn btn-primary btn-sm d-none d-sm-inline-block" role="button" href="#"><i class="fas fa-download fa-sm text-white-50"></i>&nbsp;Generate Report</a>
+    <div className="d-sm-flex justify-content-between align-items-center mb-4">
+        <h3 className="text-dark mb-0">Dashboard</h3>
     </div>
   )
 }
